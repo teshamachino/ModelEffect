2a93a1409550ec290a038af93b91f73c171df909
teshamachino <110405807+teshamachino@users.noreply.github.com>
2022-08-04 06:44:12 -0600

    update function
    binaryCalibrationMetrics.R

--- R/binaryCalibrationMetrics.R
+++ R/binaryCalibrationMetrics.R
@@ -3,7 +3,7 @@
 #' Calculates the mean predicted value and proportion of positives for a model.
 #'
 #' @param estimations The model's probability estimations.
-#' @param class The dataset's binary classification.
+#' @param labels The actual true labels from the dataset.
 #' @param bin_cnt The number of bins to calibrate the probabilities.
 #' @return A plot.
 #' @keywords probability, binary, calibration, plot, mean predicted value, proportion of positives
@@ -13,23 +13,23 @@
 #'
 #`
 
-binaryCalibrationMetrics <- function(estimations, class, bin_cnt = 10) {
+binaryCalibrationMetrics <- function(estimations, labels, bin_cnt = 10) {
 
 # package dependencies: base, stats, dplyr
 
-#get class and make the data class numeric (1,0)
-class = class %>% as.logical() %>% as.numeric()
+#get labels and make the data labels numeric (1,0)
+labels = labels %>% as.logical() %>% as.numeric()
 
 # calibration
 bin_lbl = estimations %>% dplyr::ntile(bin_cnt)
 
-#combine estimations and class by bins
-combine = data.frame(estimations, class, bin_lbl)
-combine$class = combine$class %>% as.logical()
+#combine estimations and labels by bins
+combine = data.frame(estimations, labels, bin_lbl)
+combine$labels = combine$labels %>% as.logical()
 
 #mev = mean estimated value
 #pop = proportion of positives
-calibration = combine %>% dplyr::group_by(bin_lbl) %>% dplyr::summarise(mev = mean(estimations), pop=mean(class))
+calibration = combine %>% dplyr::group_by(bin_lbl) %>% dplyr::summarise(mev = mean(estimations), pop=mean(labels))
 
 return(calibration)
 }
--- R/binaryClassificationMetrics.R
+++ R/binaryClassificationMetrics.R
@@ -3,7 +3,7 @@
 #' This function calculates classification evaluation metrics for binary outcomes.
 #'
 #' @param estimations The model probability estimations.
-#' @param class The actual true class from the dataset.
+#' @param labels The actual true labels from the dataset.
 #' @param selected_metric The selected metric(s) to pull.
 #' @return A vector of metrics.
 #' @keywords metric, model, binary, classification
@@ -13,20 +13,22 @@
 #'
 #`
 
-binaryClassificationMetrics <- function (estimations, class, selected_metric = c("ACC", "ERR", "FPR", "TPR", "FNR","TNR","PPV","NPV","F","RPP","RNP"))
+
+binaryClassificationMetrics <- function (estimations, labels, selected_metric = c("ACC", "ERR", "FPR", "TPR", "FNR","TNR","PPV","NPV","F","RPP","RNP"))
 
 {
 
-#get class(labels) and make the data class numeric (1,0)
-class = class %>% as.logical() %>% as.numeric()
+#get labels and make the data labels numeric (1,0)
+labels = labels %>% as.logical() %>% as.numeric()
 
 #sorting the probability estimations descending effectively sets a threshold
 estimations_sort_desc = sort(estimations, decreasing=TRUE)
-class_sort_desc = class[order(estimations, decreasing=TRUE)]
+labels_sort_desc = labels[order(estimations, decreasing=TRUE)]
 
 
 # using ROCR package create prediction S4 object
-pred <- ROCR::prediction(estimations, class)
+pred <- ROCR::prediction(estimations, labels)
+
 
 #pull classificaiton metrics from
 
